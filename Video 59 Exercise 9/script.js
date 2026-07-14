7c8239d967d6e6f152181e978b97874982499171
skmodishashi <skmodi.shashi@gmail.com>
2026-07-14 17:08:45 +0530

    Faulty Calculator

---
+++ Video 59 Exercise 9/script.js
@@ -0,0 +1,26 @@
+
+let random = Math.random();
+
+console.log(random)
+let a = +prompt("Enter 1st number:");
+let b = +prompt("Enter 2nd number:");
+
+
+
+if(random<0.1){
+function sum(a,b) {return a-b};
+function sub(a,b) {return a/b};
+function mul(a,b) {return a+b};
+function div(a,b) {return a**b};
+}
+else{
+function sum(a,b) {return a+b};
+function sub(a,b) {return a-b};
+function mul(a,b) {return a*b};
+function div(a,b) {return a/b};
+}
+
+console.log(sum(a,b));
+console.log(sub(a,b));
+console.log(mul(a,b));
+console.log(div(a,b));
