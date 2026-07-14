e056524042c3be315b364617e18dafa3e15258a9
skmodishashi <skmodi.shashi@gmail.com>
2026-07-14 22:15:21 +0530

    String

---
+++ Video 60/script.js
@@ -0,0 +1,26 @@
+
+console.log("Hello Everyone");
+
+let a = "Hero";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+
+console.log(a.length);
+
+let real_name = "Sk";
+let friend = "shashi";
+console.log("His name is "+real_name+" and his friend name is "+friend);
+console.log(`His name is ${real_name} and his friend name is ${friend}`);  //Template Literals
+
+let b = "Shivam";
+console.log(b.toUpperCase);
+console.log(b.toLowerCase);
+console.log(b.length);
+console.log(b.slice(1,5));
+console.log(b.slice(1));
+
+console.log(b.replace("Sh","77"));
+
+console.log(b.concat(a, "Aman", "Saurabh")); // i can add by  + method
