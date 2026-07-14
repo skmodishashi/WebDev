406835057081d367fc878aca058170defc63e937
skmodishashi <skmodi.shashi@gmail.com>
2026-07-14 16:06:25 +0530

    function

---
+++ Video 58/script.js
@@ -0,0 +1,23 @@
+
+function hello(name){
+    console.log("Hey " + name + " You are nice!")
+    console.log("Hey " + name + " You are good!")
+    console.log("Hey " + name + " You are fantastic!")
+}
+
+hello("Sk");
+
+
+function sum(a,b){
+    return a+b;
+}
+
+result = sum(2,3);
+console.log("The sum is : "+result);
+
+
+const func11 = (x)=>{
+    console.log("I am an arrow function",x)
+}
+
+func11(20);
