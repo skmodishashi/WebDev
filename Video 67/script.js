6789be4ec1d165c1ba09dccabb378e4eb992ab12
skmodishashi <skmodi.shashi@gmail.com>
2026-07-16 22:43:18 +0530

    DOM - parent,child,sibling

---
+++ Video 67/script.js
@@ -0,0 +1,12 @@
+console.log("Hello")
+console.log(document.body)
+console.log(document.body.childNodes)
+
+
+document.body.firstElementChild
+document.body.firstElementChild.parentElement
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children
+
+document.body.firstElementChild.children[3].nextElementSibling
+document.body.firstElementChild.children[3].previousElementSibling
